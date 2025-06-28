136f2bf60ef20e3d5b905b1ff72d5d112be244a0
Paul <odirahchukwumma28@gmail.com>
2025-06-28 23:17:10 +0100

    fix: Correct API_BASE URL for production environment

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,7 @@
 
 const API_BASE =
   import.meta.env.MODE === "production"
-    ? "https://170.9.234.156.com/api"
+    ? "http://170.9.234.156/api"
     : "http://localhost:8000/api";
 
 export default API_BASE;
